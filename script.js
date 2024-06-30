0b2cc4dae7390c80672f9ca6196c5060726e4608
TechPandaPro <javaisntcoffee@gmail.com>
2024-06-29 23:42:46 -0700

    Add eyes following mouse, add pet spin ability

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
-// const img = new Image();
+let spinDeg = 0;
 
 const images = {
   ellipse: { image: null, imageRatio: null },
@@ -26,45 +26,80 @@ for (const imageKey in images) {
       imageObj.image = image;
       imageObj.imageRatio = image.height / image.width;
       loadedCount++;
-      if (loadedCount === Object.keys(images).length) start();
+      if (loadedCount === Object.keys(images).length) {
+        setInterval(update, 16);
+        draw();
+      }
     },
     { once: true }
   );
   image.src = `pet_components/${imageKey}.svg`;
 }
 
-function start() {
+let mouseX;
+let mouseY;
+
+document.addEventListener("mousemove", (e) => {
+  mouseX = e.clientX;
+  mouseY = e.clientY;
+});
+
+function update() {
+  // TODO: improve update function, this is just an example
+  spinDeg += 0.5;
+}
+
+function draw() {
+  ctx.save();
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
   const petShape = images[petShapes[1]];
   const eyesBack = images.eyes_back;
   const eyesFront = images.eyes_front;
   const mouth = images.mouth;
+
+  const spinRad = spinDeg * (Math.PI / 180);
 
   const petSizeWidth = 200;
   const petSizeHeight = petShape.imageRatio * petSizeWidth;
   const petX = 50;
   const petY = 50;
 
+  const petCenterX = petX + petSizeWidth / 2;
+  const petCenterY = petY + petSizeHeight / 2;
+
   const eyesBackWidth = petSizeWidth / 2.7;
   const eyesBackHeight = eyesBack.imageRatio * eyesBackWidth;
   const eyesBackX = petX + petSizeWidth / 2 - eyesBackWidth / 2;
   const eyesBackY = petY + petSizeHeight / 2.3 - eyesBackHeight / 2;
 
+  let eyeRadians = Math.atan2(
+    mouseY - (eyesBackY + eyesBackHeight / 2),
+    mouseX - (eyesBackX + eyesBackWidth / 2)
+  );
+
+  eyeRadians = eyeRadians - spinRad;
+
+  if (Number.isNaN(eyeRadians)) eyeRadians = 225 * (Math.PI / 180);
+
   const radius = 5;
-  const eyeAngle = 225;
-  const eyeOffsetX = radius * Math.cos((Math.PI / 180) * eyeAngle);
-  const eyeOffsetY = radius * Math.sin((Math.PI / 180) * eyeAngle);
+  const eyeOffsetX = radius * Math.cos(eyeRadians);
+  const eyeOffsetY = radius * Math.sin(eyeRadians);
 
   const eyesFrontWidth = eyesBackWidth;
   const eyesFrontHeight = eyesFront.imageRatio * eyesFrontWidth;
   const eyesFrontX = eyesBackX + eyeOffsetX;
   const eyesFrontY = eyesBackY + eyeOffsetY;
-  // const eyesFrontX = eyesBackX - 3;
-  // const eyesFrontY = eyesBackY - 3;
 
   const mouthWidth = petSizeWidth / 13;
   const mouthHeight = mouth.imageRatio * mouthWidth;
   const mouthX = eyesBackX + eyesBackWidth / 2 - mouthWidth / 2;
   const mouthY = eyesBackY + 42;
+
+  ctx.translate(petCenterX, petCenterY);
+  ctx.rotate(spinRad);
+  ctx.translate(-petCenterX, -petCenterY);
 
   ctx.drawImage(petShape.image, petX, petY, petSizeWidth, petSizeHeight);
 
@@ -85,6 +120,10 @@ function start() {
   );
 
   ctx.drawImage(mouth.image, mouthX, mouthY, mouthWidth, mouthHeight);
+
+  ctx.restore();
+
+  window.requestAnimationFrame(draw);
 }
 
 function resizeCanvas() {
